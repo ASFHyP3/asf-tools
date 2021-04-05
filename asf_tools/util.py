67495222d00a8236d55bc000ffe6378e530e7065
Joseph H Kennedy <me@jhkennedy.org>
2021-04-05 14:55:13 -0800

    update util var names

--- asf_tools/util.py
+++ asf_tools/util.py
@@ -3,60 +3,61 @@ from typing import Tuple
 import numpy as np
 
 
-def tile_array(a: np.ndarray, tile_shape: Tuple[int, int] = (200, 200), pad: float = None):
+def tile_array(array: np.ndarray, tile_shape: Tuple[int, int] = (200, 200), pad: float = None):
     """Tile a 2D numpy array
 
     Turn a 2D numpy array like:
-        >>> a = [[0,0,1,1],
-                 [0,0,1,1],
-                 [2,2,3,3],
-                 [2,2,3,3]]
-        >>> a.shape
-        (4,4)
+        >>> array = [[0, 0, 1, 1],
+        ...          [0, 0, 1, 1],
+        ...          [2, 2, 3, 3],
+        ...          [2, 2, 3, 3]]
+        >>> array.shape
+        (4, 4)
 
     into a tiled array like:
-        >>> t = tiled_array(a, 2, 2)
-        >>> t = [[[0,0],
-                  [0,0]],
-                 [[1,1],
-                  [1,1]],
-                 [[2,2],
-                  [2,2]],
-                 [[3,3],
-                  [3,3]]]
-        >>> t.shape
+        >>> tiles = tiled_array(array, 2, 2)
+        >>> print(tiles)
+        [[[0, 0],
+          [0, 0]],
+         [[1, 1],
+          [1, 1]],
+         [[2, 2],
+          [2, 2]],
+         [[3, 3],
+          [3, 3]]]
+        >>> tiles.shape
         (4, 2, 2)
 
     Args:
-        a: 2D array to tile
+        array: 2D array to tile
         tile_shape: the shape of each tile
         pad: right-bottom pad `a` with `pad` as needed so `a` is evenly divisible into tiles
 
     Returns:
-        tiled: the tiled array
+        the tiled array
     """
-    ar, ac = a.shape
-    tr, tc = tile_shape
+    array_rows, array_columns = array.shape
+    tile_rows, tile_columns = tile_shape
 
-    if rmod := ar % tr:
-        rpad = tr - rmod
+    if rmod := array_rows % tile_rows:
+        rpad = tile_rows - rmod
     else:
         rpad = 0
 
-    if cmod := ac % tc:
-        cpad = tc - cmod
+    if cmod := array_columns % tile_columns:
+        cpad = tile_columns - cmod
     else:
         cpad = 0
 
     if rmod or cmod:
         if pad is None:
-            raise ValueError(f'Cannot evenly tile a {a.shape} array into ({tr},{tc}) tiles')
+            raise ValueError(f'Cannot evenly tile a {array.shape} array into ({tile_rows},{tile_columns}) tiles')
         else:
-            a = np.pad(a, ((0, rpad), (0, cpad)), constant_values=pad)
+            array = np.pad(array, ((0, rpad), (0, cpad)), constant_values=pad)
 
     tile_list = []
-    for rows in np.vsplit(a, range(tr, ar, tr)):
-        tile_list.extend(np.hsplit(rows, range(tc, ac, tc)))
+    for rows in np.vsplit(array, range(tile_rows, array_rows, tile_rows)):
+        tile_list.extend(np.hsplit(rows, range(tile_columns, array_columns, tile_columns)))
     tiled = np.moveaxis(np.dstack(tile_list), -1, 0)
     return tiled
 
@@ -65,25 +66,26 @@ def untile_array(tiled_array, array_shape: Tuple[int, int]):
     """Untile a tiled array into a 2D numpy array
 
     This is the reverse of `tile_array` and will turn a tiled array like:
-        >>> t = [[[0,0],
-                  [0,0]],
-                 [[1,1],
-                  [1,1]],
-                 [[2,2],
-                  [2,2]],
-                 [[3,3],
-                  [3,3]]]
+        >>> tiled_array = [[[0,0],
+        ...                 [0,0]],
+        ...                [[1,1],
+        ...                 [1,1]],
+        ...                [[2,2],
+        ...                 [2,2]],
+        ...                [[3,3],
+        ...                 [3,3]]]
         >>> tiled_array.shape
         (4, 2, 2)
 
     into a 2D array like:
-        >>> a = untile_array(t)
-        >>> a = [[0,0,1,1],
-                 [0,0,1,1],
-                 [2,2,3,3],
-                 [2,2,3,3]]
-        >>> a.shape
-        (4,4)
+        >>> array = untile_array(tiled_array)
+        >>> print(array)
+        [[0, 0, 1, 1],
+         [0, 0, 1, 1],
+         [2, 2, 3, 3],
+         [2, 2, 3, 3]]
+        >>> array.shape
+        (4, 4)
 
     Args:
         tiled_array: a tiled array
@@ -92,23 +94,24 @@ def untile_array(tiled_array, array_shape: Tuple[int, int]):
             to evenly tile, and the padding will be removed.
 
     Returns:
-        untiled: the untiled array
+        the untiled array
     """
-    nt, tr, tc = tiled_array.shape
-    ar, ac = array_shape
+    _, tile_rows, tile_columns = tiled_array.shape
+    array_rows, array_columns = array_shape
 
-    nr = int(np.ceil(ar / tr))
-    nc = int(np.ceil(ac / tc))
+    untiled_rows = int(np.ceil(array_rows / tile_rows))
+    untiled_columns = int(np.ceil(array_columns / tile_columns))
 
-    untiled = np.zeros((nr*tr, nc*tc), dtype=tiled_array.dtype)
+    untiled = np.zeros((untiled_rows*tile_rows, untiled_columns*tile_columns), dtype=tiled_array.dtype)
 
-    if ar > untiled.shape[0] or ac > untiled.shape[1]:
+    if array_rows > untiled.shape[0] or array_columns > untiled.shape[1]:
         raise ValueError(
             f'array_shape {array_shape} must be the same or smaller than the untiled array {untiled.shape}'
         )
 
-    for ii in range(nr):
-        for jj in range(nc):
-            untiled[ii*tr:(ii+1)*tr, jj*tc:(jj+1)*tc] = tiled_array[ii * nr + jj, :, :]
+    for ii in range(untiled_rows):
+        for jj in range(untiled_columns):
+            untiled[ii*tile_rows:(ii+1)*tile_rows, jj*tile_columns:(jj+1)*tile_columns] = \
+                tiled_array[ii * untiled_rows + jj, :, :]
 
-    return untiled[:ar, :ac]
+    return untiled[:array_rows, :array_columns]
