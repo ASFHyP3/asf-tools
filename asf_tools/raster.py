c7d7d9eaf3ae3e97db6619f5b61dfc0ae7399ff6
Joseph H Kennedy <me@jhkennedy.org>
2021-04-14 23:20:54 -0800

    Add a scale conversion function

---
+++ asf_tools/raster.py
@@ -0,0 +1,29 @@
+import warnings
+import numpy as np
+
+
+def convert_scale(array: np.ndarray, in_scale: str, out_scale: str) -> np.ndarray:
+    """Convert raster scale between power, amplitude and db"""
+    if in_scale == out_scale:
+        warnings.warn(f'Nothing to do! {in_scale} is same as {out_scale}.')
+        return array
+
+    if in_scale == 'db':
+        if out_scale == 'power':
+            return 10 ** (array / 10)
+        if out_scale == 'amplitude':
+            return 10 ** (array / 20)
+
+    if in_scale == 'amplitude':
+        if out_scale == 'power':
+            return array ** 2
+        if out_scale == 'db':
+            return 10 * np.log10(array ** 2)
+
+    if in_scale == 'power':
+        if out_scale == 'amplitude':
+            return np.sqrt(array)
+        if out_scale == 'db':
+            return 10 * np.log10(array)
+
+    raise ValueError(f'Cannot convert raster of scale {in_scale} to {out_scale}')
---
+++ tests/test_raster.py
@@ -0,0 +1,40 @@
+import numpy as np
+
+import pytest
+
+from asf_tools import raster
+
+def test_convert_scale():
+    c = raster.convert_scale(np.array([-10, -5, 0, 5, 10]), 'amplitude', 'power')
+    assert np.all(np.isclose(np.array([100, 25, 0, 25, 100]), c))
+
+    c = raster.convert_scale(np.array([-10, -5, 0, 5, 10]), 'amplitude', 'db')
+    assert np.all(np.isclose(np.array([20., 13.97940009, -np.inf, 13.97940009, 20.]), c))
+
+    c = raster.convert_scale(np.array([-1, 0, 1, 4, 9]), 'power', 'amplitude')
+    assert np.isnan(c[0])
+    assert np.all(np.isclose(np.array([0, 1, 2, 3]), c[1:]))
+
+    c = raster.convert_scale(np.array([-1, 0, 1, 4, 9]), 'power', 'db')
+    assert np.isnan(c[0])
+    assert np.all(np.isclose(np.array([-np.inf, 0., 6.02059991, 9.54242509]), c[1:]))
+
+    c = raster.convert_scale(np.array([np.nan, -np.inf, 0., 6.02059991, 9.54242509]), 'db', 'power')
+    assert np.isnan(c[0])
+    assert np.all(np.isclose(np.array([0, 1, 4, 9]), c[1:]))
+
+    c = raster.convert_scale(np.array([-np.inf, -20., 0., 13.97940009, 20.]), 'db', 'amplitude')
+    assert np.all(np.isclose(np.array([0., 0.1, 1., 5., 10.]), c))
+
+    a = np.array([-10, -5, 0, 5, 10])
+    with pytest.raises(ValueError):
+        _ = raster.convert_scale(a, 'power', 'foo')
+    with pytest.raises(ValueError):
+        _ = raster.convert_scale(a, 'bar', 'amplitude')
+
+    with pytest.warns(UserWarning):
+        assert np.all(np.array([-10, -5, 0, 5, 10]) == raster.convert_scale(a, 'amplitude', 'amplitude'))
+    with pytest.warns(UserWarning):
+        assert np.all(np.array([-10, -5, 0, 5, 10]) == raster.convert_scale(a, 'power', 'power'))
+    with pytest.warns(UserWarning):
+        assert np.all(np.array([-10, -5, 0, 5, 10]) == raster.convert_scale(a, 'db', 'db'))
