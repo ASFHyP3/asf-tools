54c77d824679be751f8726c5a4140c6d3f9e6386
Joseph H Kennedy <me@jhkennedy.org>
2021-06-08 16:14:24 -0800

    Remove unecessary logical_and

--- asf_tools/water_map.py
+++ asf_tools/water_map.py
@@ -98,7 +98,7 @@ def min_max_membership(array: np.ndarray, lower_limit: float, upper_limit: float
     return membership
 
 
-def segment_area_membership(segments: np.ndarray, initial_mask: np.ndarray, min_area: int = 3, max_area: int = 10) -> \
+def segment_area_membership(segments: np.ndarray,min_area: int = 3, max_area: int = 10) -> \
         np.ndarray:
     segment_areas = np.bincount(segments.ravel())
 
@@ -111,8 +111,7 @@ def segment_area_membership(segments: np.ndarray, initial_mask: np.ndarray, min_
     np.putmask(segment_membership, np.isin(segment_membership, segments_above_threshold), 1)
 
     for area in possible_areas:
-        possible_pixels = np.isin(segments, (segment_areas == area).nonzero())
-        mask = np.logical_and(possible_pixels, initial_mask)
+        mask = np.isin(segments, (segment_areas == area).nonzero())
         np.putmask(segment_membership, mask, fuzz.interp_membership(possible_areas, activation, area))
     return segment_membership
 
@@ -122,7 +121,7 @@ def fuzzy_refinement(initial_map: np.ndarray, gaussian_array: np.ndarray, hand_a
     water_map = np.ones_like(initial_map)
 
     water_segments = measure.label(initial_map, connectivity=2)
-    water_segment_membership = segment_area_membership(water_segments, initial_map)
+    water_segment_membership = segment_area_membership(water_segments)
     water_map &= ~np.isclose(water_segment_membership, 0.)
 
     gaussian_membership = min_max_membership(gaussian_array, gaussian_thresholds[0], gaussian_thresholds[1], 0.005)
