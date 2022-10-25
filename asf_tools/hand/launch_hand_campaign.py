8a510c0d0ed7347d5d8f831c9f7ff7883747eb60
jacquelynsmale <34557291+jacquelynsmale@users.noreply.github.com>
2022-10-25 10:51:22 -0800

    Fix parameter specs

--- asf_tools/hand/launch_hand_campaign.py
+++ asf_tools/hand/launch_hand_campaign.py
@@ -1,7 +1,7 @@
 import boto3
 from asf_tools.vector import get_features
 
-batch = boto3.client('batch')
+batch = boto3.client('batch', region_name='us-west-2')
 
 # TODO: Update revision number for job definition
 JOB_QUEUE = 'BatchJobQueue-vSh6SePm97I5ELZr'
@@ -12,12 +12,12 @@ tile_features = get_features(HAND_GEOJSON)
 
 for feature in tile_features:
     dem_tile_url = feature.GetFieldAsString(0)
-
+    
     batch.submit_job(
-        job_name=dem_tile_url.split('/')[-2][:-4],
-        job_queue=JOB_QUEUE,
+        jobName=dem_tile_url.split('/')[-2][:-4],
+        jobQueue=JOB_QUEUE,
         jobDefinition=JOB_DEFINITION,
         parameters={
-            'dem_tile': dem_tile_url,
+            'dem_tile_url': dem_tile_url,
         },
     )
