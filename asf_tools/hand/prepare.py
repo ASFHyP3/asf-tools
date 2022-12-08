1f4190109bda83cfdf54c250ffd5f0cf4eb8e0a3
jacquelynsmale <34557291+jacquelynsmale@users.noreply.github.com>
2022-12-08 00:26:15 -0900

    correct geojson name

--- asf_tools/hand/prepare.py
+++ asf_tools/hand/prepare.py
@@ -12,7 +12,7 @@ from asf_tools import vector
 from asf_tools.composite import get_epsg_code
 from asf_tools.util import GDALConfigManager
 
-HAND_GEOJSON = '/vsicurl/https://glo-30-hand.s3.amazonaws.com/v1/2021/glo30-hand.geojson'
+HAND_GEOJSON = '/vsicurl/https://glo-30-hand.s3.amazonaws.com/v1/2021/glo-30-hand.geojson'
 
 gdal.UseExceptions()
 ogr.UseExceptions()
