bdebdce13341998823ea9af28dfcfaa535c9651a
Jacquelyn Smale <34557291+jacquelynsmale@users.noreply.github.com>
2023-02-01 14:44:48 -0900

    Update src/asf_tools/water_map.py
    Co-authored-by: Jake Herrmann <jtherrmann@alaska.edu>

--- src/asf_tools/water_map.py
+++ src/asf_tools/water_map.py
@@ -391,10 +391,10 @@ def hyp3():
     log.info(f'Water map created successfully: {water_map_raster}')
 
     files_to_remove = [
-        Path(product_name) / f'{product_name}_WM_VV_initial.tif',
-        Path(product_name) / f'{product_name}_WM_VH_initial.tif',
-        Path(product_name) / f'{product_name}_WM_VV_fuzzy.tif',
-        Path(product_name) / f'{product_name}_WM_VH_fuzzy.tif',
+        Path(product_name) / f'{product_name}_VV_initial.tif',
+        Path(product_name) / f'{product_name}_VH_initial.tif',
+        Path(product_name) / f'{product_name}_VV_fuzzy.tif',
+        Path(product_name) / f'{product_name}_VH_fuzzy.tif',
     ]
     for file_to_remove in files_to_remove:
         file_to_remove.unlink()
