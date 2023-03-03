7fc6b9e7eaab9da0ed8891c951c3d5a5c9943de9
Forrest Williams <forrestfwilliams@icloud.com>
2023-03-03 14:35:07 +0000

    update naming and don't assume nodata value

--- src/asf_tools/flood_map.py
+++ src/asf_tools/flood_map.py
@@ -125,11 +125,15 @@ def estimate_flood_depth(label, hand, flood_labels, estimator='iterative', water
     return hand_mean + water_level_sigma * hand_std
 
 
-def read_mask(raster: Union[str, Path]):
+def get_valid_data_mask(raster: Union[str, Path]):
     ds = gdal.Open(str(raster))
     band = ds.GetRasterBand(1)
-    mask = band.GetMaskBand().ReadAsArray().astype(np.bool_)
-    return mask
+    nodata_value = band.GetNoDataValue()
+    data = band.GetMaskBand().ReadAsArray()
+    valid_data = np.ones(data.shape).asytpe(np.bool_)
+    valid_data[data == nodata_value] = False
+    return valid_data
+
 
 def make_flood_map(out_raster: Union[str, Path],  vv_raster: Union[str, Path],
                    water_raster: Union[str, Path], hand_raster: Union[str, Path],
@@ -218,9 +222,9 @@ def make_flood_map(out_raster: Union[str, Path],  vv_raster: Union[str, Path],
 
     # mask waterdepth, flooddepth, floodmask with mask defined in the vv raster
     nodata = -1
-    padding_mask = ~read_mask(vv_raster)
-    flood_depth[padding_mask] = nodata
-    flood_mask[padding_mask] = nodata
+    valid_data_mask = get_valid_data_mask(vv_raster)
+    flood_depth[~valid_data_mask] = nodata
+    flood_mask[~valid_data_mask] = nodata
 
     write_cog(str(out_raster).replace('.tif', f'_{estimator}_WaterDepth.tif'), flood_depth, transform=geotransform,
               epsg_code=epsg, dtype=gdal.GDT_Float64, nodata_value=nodata)
