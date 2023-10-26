80610e4635ec8f935740ef7d5785863f7dabbfdf
Joseph H Kennedy <me@jhkennedy.org>
2023-10-26 14:28:23 -0800

    fix lint

--- src/asf_tools/hydrosar/__init__.py
+++ src/asf_tools/hydrosar/__init__.py
@@ -3,12 +3,12 @@ from warnings import warn
 HYDROSAR_MOVE_WARNING = \
     """
     ---------------------------------------------------------------------------
-    The HydroSAR codes (`flood_map`, `water_map` and `hand` modules) are being 
+    The HydroSAR codes (`flood_map`, `water_map` and `hand` modules) are being
     moved to the HydroSAR project repository:
         <https://github.com/fjmeyer/hydrosar>
-    and will be provided in a new pip/conda installable package `hydrosar`. 
-    
-    The `asf_tools.hydrosar` subpackage will be removed in a future release. 
+    and will be provided in a new pip/conda installable package `hydrosar`.
+
+    The `asf_tools.hydrosar` subpackage will be removed in a future release.
     ----------------------------------------------------------------------------
     """
 
