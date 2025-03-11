1847a55c87f25309eb5b86c11ca5f191ff113ec2
Alex-Lewandowski <aflewandowski@alaska.edu>
2025-03-11 23:30:44 +0000

    update read_as_masked_array to handle nan nodata values

--- src/asf_tools/raster.py
+++ src/asf_tools/raster.py
@@ -55,17 +55,20 @@ def read_as_masked_array(raster: str | Path, band: int = 1) -> np.ma.MaskedArray
         band: The raster band to read
 
     Returns:
-        data: The raster pixel data as a numpy MaskedArray
+        masked_raster: The raster pixel data as a numpy MaskedArray
     """
     log.debug(f'Reading raster values from {raster}')
     ds = gdal.Open(str(raster))
     raster_band = ds.GetRasterBand(band)
-    data = np.ma.masked_invalid(raster_band.ReadAsArray())
+    
     nodata = raster_band.GetNoDataValue()
-    if nodata is not None:
-        return np.ma.masked_values(data, nodata)
+    if nodata and not np.isnan(nodata):
+        masked_raster = np.ma.masked_values(raster_band, nodata)
+    else:
+        masked_raster = np.ma.masked_invalid(raster_band.ReadAsArray())
+    
     del ds  # How to close w/ gdal
-    return data
+    return masked_raster
 
 
 def read_as_array(raster: str, band: int = 1) -> np.ndarray:
